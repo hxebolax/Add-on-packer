c92bbcf3e02d52b67143f27a37801bd6edc29ca2
XeBoLaX <xebolax@gmail.com>
2022-11-27 09:23:48 +0100

    Versi├│n 1.3.2

--- buildVars.py
+++ buildVars.py
@@ -35,7 +35,7 @@ addon_info = {
 
 Todas las áreas de fácil manejo y para ahorrar tiempo respecto al gestor de complementos nativo de NVDA."""),
 	# version
-	"addon_version": "1.3.1",
+	"addon_version": "1.3.2",
 	# Author(s)
 	"addon_author": u"Héctor J. Benítez Corredera <xebolax@gmail.com>",
 	# URL for the add-on documentation support
--- changelog.md
+++ changelog.md
@@ -1,3 +1,1 @@
-* Traducciones actualizadas árabe, ruso, francés, portugués, ucraniano y turco.
-
-* Resto de traducciones actualizadas automáticamente con IA.
+* Idioma Frances corregido.
